e136756c2ee90441dda32c9d50dfcf05d66ceb33
DuncanSummers <87944849+DuncanSummers@users.noreply.github.com>
2023-06-07 15:12:23 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector})})
